16d802971623d8264095cf7e117f0bb57a2312e0
Krutika-05 <krutika98@gmail.com>
2020-10-17 16:12:55 +0530

    commit

--- assignments/assignment8/my_evaluation.py
+++ assignments/assignment8/my_evaluation.py
@@ -245,51 +245,6 @@ class my_evaluation:
 
             return auc_target
 
-import numpy as np
-#from my_evaluation import my_evaluation
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from collections import Counter
-from pprint import pprint
-from copy import deepcopy
-from pdb import set_trace
-
-
-# Load training data
-data_train = pd.read_csv("..\data\Iris_train.csv")
-
-# Separate independent variables and dependent variables
-independent = ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm"]
-X = data_train[independent]
-y = data_train["Species"]
-
-# Fit model
-clf = DecisionTreeClassifier(criterion="entropy", max_depth=2)
-clf.fit(X, y)
-
-# Predict on training data
-predictions = clf.predict(X)
-
-# Predict probabilities
-probs = clf.predict_proba(X)
-probs = pd.DataFrame({key: probs[:, i] for i, key in enumerate(clf.classes_)})
-
-
-# Evaluate results
-metrics = my_evaluation(predictions, y, probs)
-result = {}
-for target in clf.classes_:
-    result[target] = {}
-    metrics.precision(target)
-    result[target]["prec"] = metrics.precision(target)
-    result[target]["recall"] = metrics.recall(target)
-    result[target]["f1"] = metrics.f1(target)
-    result[target]["auc"] = metrics.auc(target)
-print("result")
-print(result)
-f1 = {average: metrics.f1(target=None, average=average) for average in ["macro", "micro", "weighted"]}
-print("Average F1 scores: ")
-print(f1)
 
 
 
